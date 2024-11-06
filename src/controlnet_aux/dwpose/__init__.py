9bd2c03793b6ecb53800acd89c39a652515c02cd
yuzhipeng <samuelpenpen@gmail.com>
2024-11-06 14:57:51 +0800

    add dwpose/human

--- src/controlnet_aux/dwpose/__init__.py
+++ src/controlnet_aux/dwpose/__init__.py
@@ -10,15 +10,18 @@ os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 import cv2
 import json
 import torch
+import warnings
+
 import numpy as np
 from PIL import Image
+
 
 from ..util import HWC3, resize_image
 from . import util
 
 from .animalpose import AnimalPoseImage
 from .types import PoseResult, HandResult, FaceResult, AnimalPoseResult
-
+from .types import Keypoint, BodyResult
 
 from typing import Tuple, List, Callable, Union, Optional
 
@@ -28,6 +31,60 @@ def draw_animalposes(animals: list[list[Keypoint]], H: int, W: int) -> np.ndarra
         canvas = draw_animalpose(canvas, animal_pose)
     return canvas
 
+
+
+def pad64(x):
+    return int(np.ceil(float(x) / 64.0) * 64 - x)
+
+def safer_memory(x):
+    # Fix many MAC/AMD problems
+    return np.ascontiguousarray(x.copy()).copy()
+
+def common_input_validate(input_image, output_type, **kwargs):
+    if "img" in kwargs:
+            warnings.warn("img is deprecated, please use `input_image=...` instead.", DeprecationWarning)
+            input_image = kwargs.pop("img")
+    
+    if "return_pil" in kwargs:
+            warnings.warn("return_pil is deprecated. Use output_type instead.", DeprecationWarning)
+            output_type = "pil" if kwargs["return_pil"] else "np"
+    
+    if type(output_type) is bool:
+        warnings.warn("Passing `True` or `False` to `output_type` is deprecated and will raise an error in future versions")
+        if output_type:
+            output_type = "pil"
+
+    if input_image is None:
+        raise ValueError("input_image must be defined.")
+
+    if not isinstance(input_image, np.ndarray):
+        input_image = np.array(input_image, dtype=np.uint8)
+        output_type = output_type or "pil"
+    else:
+        output_type = output_type or "np"
+    
+    return (input_image, output_type)
+
+def resize_image_with_pad(input_image, resolution, upscale_method = "", skip_hwc3=False, mode='edge'):
+    if skip_hwc3:
+        img = input_image
+    else:
+        img = HWC3(input_image)
+    H_raw, W_raw, _ = img.shape
+    if resolution == 0:
+        return img, lambda x: x
+    k = float(resolution) / float(min(H_raw, W_raw))
+    H_target = int(np.round(float(H_raw) * k))
+    W_target = int(np.round(float(W_raw) * k))
+    img = cv2.resize(img, (W_target, H_target), interpolation=get_upscale_method(upscale_method) if k > 1 else cv2.INTER_AREA)
+    H_pad, W_pad = pad64(H_target), pad64(W_target)
+    img_padded = np.pad(img, [[0, H_pad], [0, W_pad], [0, 0]], mode=mode)
+
+    def remove_pad(x):
+        return safer_memory(x[:H_target, :W_target, ...])
+
+    return safer_memory(img_padded), remove_pad
+    
 
 def draw_animalpose(canvas: np.ndarray, keypoints: list[Keypoint]) -> np.ndarray:
     # order of the keypoints for AP10k and a standardized list of colors for limbs
